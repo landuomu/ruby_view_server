29feef71e173c116aa33968da8c73ab9ae2662c6
landuomu <lhchchc@gmail.com>
2015-10-18 18:06:48 +0800

    test example 1

--- erb_example.rb
+++ erb_example.rb
@@ -1,5 +1,7 @@
 require 'erb'
 
 x = 42
-template = ERB.new "The value of x is: <%= x %>"
+y = '25'.to_i
+template = ERB.new "The value of x is: <%= x + y %>"\
+									 "<%= y-x %>"
 puts template.result(binding)
